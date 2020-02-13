2e460bd94f49ac7a5ae40ed5a1c9437b5fa40301
JustinGalante1 <jn2wf@virginia.edu>
2020-02-13 15:45:45 -0500

    added some WORKING nav links to the gradetracker view

--- gradetracker/urls.py
+++ gradetracker/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-
+from django.contrib import admin
+from django.urls import include, path
 from . import views
+import personaldashboard
 
+app_name = 'gradetracker'
 urlpatterns = [
+    path('home', personaldashboard.views.HomeView.as_view(), name="home"),
     path('', views.IndexView.as_view(), name='index'),
 ]
--- personaldashboard/urls.py
+++ personaldashboard/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
+app_name = 'personaldashboard'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomeView.as_view(), name="home"),
+    path('home', views.HomeView.as_view(), name="home"),
     path('gradetracker/', include('gradetracker.urls')),
 ]
