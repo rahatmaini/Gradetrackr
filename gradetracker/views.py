ffdecf4f3caeb480a8df087ff30663a473a54326
Ttran829 <41520668+Ttran829@users.noreply.github.com>
2020-03-13 15:49:37 -0400

    added a course form

--- gradetracker/views.py
+++ gradetracker/views.py
@@ -3,16 +3,27 @@ from django.urls import reverse
 from django.views import generic
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+#new stuff
+from .models import Course, CourseForm
+#new stuff
 
 # in gradetracker directory
 # Create your views here.
 
-class AddView(TemplateView):
-    template_name = 'gradetracker/add.html'
+#class AddView(TemplateView):
+#    template_name = 'gradetracker/add.html'
 
 
 
 class IndexView(TemplateView):
     template_name = 'gradetracker/index.html'
     #if user is logged in, create a new student object with user context variable
+
+def CreateCourse(request):
+    form = CourseForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form' : form
+    }
+    return render(request, "gradetracker/add.html", context)
